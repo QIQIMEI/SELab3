7a228acd2a101a0f07bc7453c444337f007c833e
VermouthMY <shqg>
2016-06-25 14:28:26 +0800

    基础框架

---
+++ middle.js
@@ -0,0 +1,10 @@
+// switch forms
+function showAddUser() {
+    $('#admin_showuser', parent.document).hide();
+    $('#admin_adduser', parent.document).show();
+}
+
+function showShowUser() {
+    $('#admin_adduser', parent.document).hide();
+    $('#admin_showuser', parent.document).show();
+}
