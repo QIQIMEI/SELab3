3b1c4fdddff27cea249bab1780f02649badd8a0c
Allen <13302010058>
2016-06-26 16:42:24 +0800

    change files

--- meeting/WebContent/js/middle.js
+++ meeting/WebContent/js/middle.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //user
 function userShowSchedule() {
     $('#user_showSchedule', parent.document).show();
@@ -26,33 +25,4 @@ function userMyMeeting() {
     $('#user_showNotice', parent.document).hide();
     $('#user_createMeeting', parent.document).hide();
     $('#user_myMeeting', parent.document).show();
-=======
-//user
-function userShowSchedule() {
-    $('#user_showSchedule', parent.document).show();
-    $('#user_showNotice', parent.document).hide();
-    $('#user_createMeeting', parent.document).hide();
-    $('#user_myMeeting', parent.document).hide();
-}
-
-function userShowNotice() {
-    $('#user_showSchedule', parent.document).hide();
-    $('#user_showNotice', parent.document).show();
-    $('#user_createMeeting', parent.document).hide();
-    $('#user_myMeeting', parent.document).hide();
-}
-
-function userCreateMeeting() {
-    $('#user_showSchedule', parent.document).hide();
-    $('#user_showNotice', parent.document).hide();
-    $('#user_createMeeting', parent.document).show();
-    $('#user_myMeeting', parent.document).hide();
-}
-
-function userMyMeeting() {
-    $('#user_showSchedule', parent.document).hide();
-    $('#user_showNotice', parent.document).hide();
-    $('#user_createMeeting', parent.document).hide();
-    $('#user_myMeeting', parent.document).show();
->>>>>>> 87fb6ec7aa9353e3c380a7757251879fa1575297
 }
